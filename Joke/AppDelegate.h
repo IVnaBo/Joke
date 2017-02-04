9b4eb44da771fb8213c360607a7b5863d4bfec75
BOBO <bobo@bobodemacbook-pro.local>
2017-02-04 16:11:37 +0800

    Initial Commit

---
+++ Joke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Joke
+//
+//  Created by BOBO on 17/2/4.
+//  Copyright © 2017年 BOBO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
